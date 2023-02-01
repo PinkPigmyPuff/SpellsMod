37a863d11692101ce8c3179d0925ee1d3cdd0d98
jsaperr <urawizardharry21savage@gmail.com>
2023-02-01 09:35:07 -0800

    raycast computation ig

--- src/main/java/com/jasperpuffinv/spellsmod/item/custom/BeginnerStaff.java
+++ src/main/java/com/jasperpuffinv/spellsmod/item/custom/BeginnerStaff.java
@@ -4,7 +4,10 @@ import com.jasperpuffinv.spellsmod.Main;
 import com.jasperpuffinv.spellsmod.entity.projectiles.ExplodingProjectileEntity;
 import com.jasperpuffinv.spellsmod.gui.BeginnerGui;
 import com.jasperpuffinv.spellsmod.gui.BeginnerScreen;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -12,6 +15,10 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class BeginnerStaff extends Item {
@@ -21,20 +28,41 @@ public class BeginnerStaff extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        MinecraftClient client = MinecraftClient.getInstance();
         Main.LOGGER.info("item oh yeah");
         if (user.isSneaking()) {
-            //MinecraftClient.getInstance().setScreen(new BeginnerScreen(new BeginnerGui())); temp i am testing projectile heehee
+            MinecraftClient.getInstance().setScreen(new BeginnerScreen(new BeginnerGui()));
+            /*
             world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_FIREWORK_ROCKET_SHOOT, SoundCategory.NEUTRAL, 0.5F, 1F);
             user.getItemCooldownManager().set(this, 10);
             ExplodingProjectileEntity explodeEntity = new ExplodingProjectileEntity(world, user);
             explodeEntity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1.5F, 1.0F);
             world.spawnEntity(explodeEntity);
+             */
 
         } else {
             if (BeginnerGui.isExplode) {
-                world.createExplosion(null, user.getX(), user.getY(), user.getZ(), 4f, World.ExplosionSourceType.BLOCK);
-                Main.LOGGER.info("explode");
-                user.getItemCooldownManager().set(this, 20);
+                assert client.player != null;
+                // Raycast a line from the player, and store the HitData
+                HitResult hit = user.raycast(300, 1, true);
+                switch (hit.getType()) {
+                    case MISS -> client.player.networkHandler.sendChatMessage("Missed!");
+                    case BLOCK -> {
+                        BlockHitResult blockHit = (BlockHitResult) hit;
+                        BlockPos pos = blockHit.getBlockPos();
+                        BlockState blockState = client.world.getBlockState(pos);
+                        Block block = blockState.getBlock();
+                        String message = block.toString() + " at: " + pos.toString();
+                        client.player.networkHandler.sendChatMessage(message);
+                    }
+                    case ENTITY -> {
+                        EntityHitResult entityHit = (EntityHitResult) hit;
+                        Entity entity = entityHit.getEntity();
+                        BlockPos entityBlockPos = entity.getBlockPos();
+                        String EMessage = entity + "at: " + entityBlockPos.toString();
+                        client.player.networkHandler.sendChatMessage(EMessage);
+                    }
+                }
             } else if (BeginnerGui.isLaunch) {
                 user.addVelocity(0, 1, 0);
                 Main.LOGGER.info("launch");
